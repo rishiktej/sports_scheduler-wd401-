fad70cb574ee499bb7c2ba59f79ef9a1fb1aceb2
rishiktej <ugs21032_cic.tej@cbit.org.in>
2024-04-24 21:38:36 +0530

    added docker

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 const app = require("./app");
+const Sentry = require("@sentry/node");
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import express from "express";
 
-app.listen(4000, () => {
-  console.log("Started express server at port 4000");
+Sentry.init({
+  dsn: "https://c0fa10d459de17f39cca2ed77476a3e6@o4506938780418048.ingest.us.sentry.io/4507142324879360",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
 });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// All your controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+app.listen(4000);
